7f7b3b10cf6efdcd54f9e786e7f40e4bbdc8463c
Francesca Infranca <54380721+francescainfranca@users.noreply.github.com>
2021-04-25 21:26:43 -0500

    Copy contents into project repo & add/commit/push

---
+++ src/main/java/com/infranca/francesca/bigdata/App.java
@@ -0,0 +1,13 @@
+package com.infranca.francesca.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
